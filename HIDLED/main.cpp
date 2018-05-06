b115ec4e44eb338e51fe9ecdc62e5b4b6e9b8632
Pietro Saccardi <lizardm4@gmail.com>
2018-05-06 13:57:05 +0200

    Initial Commit

---
+++ HIDLED/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  HIDLED
+//
+//  Created by Pietro Saccardi on 06/05/2018.
+//  Copyright © 2018 Pietro Saccardi. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
