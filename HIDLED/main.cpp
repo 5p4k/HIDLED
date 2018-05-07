5992041fee2a19d7b210653ce17478b47b653e57
Pietro Saccardi <lizardm4@gmail.com>
2018-05-07 02:08:20 +0200

    Writing main routine and cmd line parsing.

--- HIDLED/main.cpp
+++ HIDLED/main.cpp
@@ -7,30 +7,215 @@
 //
 
 #include <iostream>
+#include <sstream>
 #include "hid.hpp"
 
-int main(int argc, const char * argv[]) {
-    for (auto device : spak::hid_device_enumerator(kHIDPage_GenericDesktop, kHIDUsage_GD_Keyboard)) {
-        std::cout << "Device " << device.manufacturer() << " >> " << device.product();
+void list() {
+    for (auto &device : spak::hid_device_enumerator(kHIDPage_GenericDesktop, kHIDUsage_GD_Keyboard)) {
+        std::string const manu = device.manufacturer();
+        std::string const prod = device.product();
+        std::cout << "Device ";
+        if (prod.empty()) {
+            std::cout << "<unknown>";
+        } else {
+            std::cout << "'" << prod << "'";
+        }
+        std::cout << " by ";
+        if (manu.empty()) {
+            std::cout << "<unknown>";
+        } else {
+            std::cout << "'" << manu << "'";
+        }
+        
         spak::hid_device_opener opener = device.open();
         if (not opener.is_open()) {
             std::cout << " (can't be opened: " << spak::describe_io_return(opener.result()) << ")";
         }
         std::cout << std::endl;
-        for (auto element : device.elements(kHIDPage_LEDs)) {
-            std::cout << "   Element " << element.name() << " [" << element.logical_min() << ".." << element.logical_max() << "]";
+        
+        std::size_t elm_idx = 0;
+        for (auto &element : device.elements(kHIDPage_LEDs)) {
+            std::cout << "    Element " << elm_idx ;
+            std::string const name = element.name();
+            if (not name.empty()) {
+                std::cout << " \"" << name << "\"";
+            }
+            std::cout << " [" << element.logical_min() << ".." << element.logical_max() << "]";
             if (opener.is_open()) {
-                std::cout << ": " << element.value<CFIndex>();
+                try {
+                    CFIndex const value = element.value<CFIndex>();
+                    std::cout << ": " << value;
+                } catch (...) {
+                    // Pass
+                }
             }
             std::cout << std::endl;
-        }
-        if (device.manufacturer().find("Apple") != std::string::npos and opener.is_open()) {
-            auto elements = device.elements(kHIDPage_LEDs);
-            if (elements.size() >= 2) {
-                std::cout << "Setting LED 1 of Apple keyboard to 1." << std::endl;
-                elements[1].value<CFIndex>() = 1;
-            }
+            ++elm_idx;
         }
     }
-    return 0;
+}
+
+std::unique_ptr<spak::hid_device> match_keyboard(std::string const &match_prod = "", std::string const &match_manu = "") {
+    for (auto device : spak::hid_device_enumerator(kHIDPage_GenericDesktop, kHIDUsage_GD_Keyboard)) {
+        std::string const manu = device.manufacturer();
+        std::string const prod = device.product();
+        if (not match_manu.empty() and manu != match_manu) {
+            continue;
+        }
+        if (not match_prod.empty() and prod != match_prod) {
+            continue;
+        }
+        return std::make_unique<spak::hid_device>(std::move(device));
+    }
+    return nullptr;
+}
+
+
+void help() {
+    std::cout << "Usage: <program> --help" << std::endl;
+    std::cout << "       <program> [--list]" << std::endl;
+    std::cout << "       <program> [--product <product>] [--manufacturer <manufacturer>] --toggle <led_idx>" << std::endl;
+    std::cout << "       <program> [--product <product>] [--manufacturer <manufacturer>] --set <led_idx> <value>" << std::endl;
+}
+
+
+struct cmdline {
+    
+    enum struct actions {
+        list,
+        toggle,
+        set,
+        help,
+        wrong_cmd_line
+    };
+    
+    actions action;
+    std::string match_manufacturer;
+    std::string match_product;
+    std::size_t element;
+    CFIndex value;
+    
+    cmdline() : action(actions::list), element(std::numeric_limits<std::size_t>::max()), value(0) {}
+    
+    void parse(int argc, const char * argv[]) {
+        for (int argn = 1; argn < argc; ++argn) {
+            std::string const arg = argv[argn];
+            if (arg == "-h" or arg == "--help") {
+                action = actions::help;
+            } else if (arg == "-l" or arg == "--list") {
+                action = actions::list;
+            } else if (arg == "-p" or arg == "--product") {
+                if (argn >= argc - 1) {
+                    std::cerr << "Missing argument 'product' at position " << argn + 1 << std::endl;
+                    action = actions::wrong_cmd_line;
+                    continue;
+                }
+                match_product = argv[++argn];
+            } else if (arg == "-m" or arg == "--manufacturer") {
+                if (argn >= argc - 1) {
+                    std::cerr << "Missing argument 'manufacturer' at position " << argn + 1 << std::endl;
+                    action = actions::wrong_cmd_line;
+                    continue;
+                }
+                match_manufacturer = argv[++argn];
+            } else if (arg == "-s" or arg == "--set") {
+                if (argn >= argc - 1) {
+                    std::cerr << "Missing argument 'element index' at position " << argn + 1 << std::endl;
+                    action = actions::wrong_cmd_line;
+                    continue;
+                }
+                std::stringstream ss_idx(argv[++argn]);
+                ss_idx >> element;
+                if (argn >= argc - 1) {
+                    std::cerr << "Missing argument 'value' at position " << argn + 1 << std::endl;
+                    action = actions::wrong_cmd_line;
+                    continue;
+                }
+                std::stringstream ss_val(argv[++argn]);
+                ss_val >> value;
+            } else if (arg == "-t" or arg == "--toggle") {
+                if (argn >= argc - 1) {
+                    std::cerr << "Missing argument 'element index' at position " << argn + 1 << std::endl;
+                    action = actions::wrong_cmd_line;
+                    continue;
+                }
+                std::stringstream ss_idx(argv[++argn]);
+                ss_idx >> element;
+            } else {
+                std::cerr << "Unknown switch or argument '" << arg << "' at position " << argn << std::endl;
+                action = actions::wrong_cmd_line;
+            }
+        }
+        
+    }
+};
+
+
+namespace return_code {
+    static constexpr int ok = 0;
+    static constexpr int cmdline_error = 1;
+    static constexpr int keyboard_not_found = 2;
+    static constexpr int cannot_open_device = 3;
+    static constexpr int led_not_found = 4;
+    static constexpr int unknown_error = 5;
+}
+
+
+int main(int argc, const char * argv[]) {
+    try {
+        cmdline cmd;
+        cmd.parse(argc, argv);
+        switch (cmd.action) {
+            case cmdline::actions::wrong_cmd_line:
+                help();
+                return return_code::cmdline_error;
+            case cmdline::actions::help:
+                help();
+                return return_code::ok;
+            case cmdline::actions::list:
+                list();
+                return return_code::ok;
+            case cmdline::actions::set:
+                [[fallthrough]];
+            case cmdline::actions::toggle: {
+                auto p_device = match_keyboard(cmd.match_product, cmd.match_manufacturer);
+                if (p_device == nullptr) {
+                    std::cerr << "Unable to find a keyboard matching";
+                    if (not cmd.match_product.empty()) {
+                        std::cerr << " product '" << cmd.match_product << "'";
+                    }
+                    if (not cmd.match_manufacturer.empty()) {
+                        std::cerr << " manufacturer '" << cmd.match_manufacturer << "'";
+                    }
+                    std::cerr << "." << std::endl;
+                    return return_code::keyboard_not_found;
+                }
+                spak::hid_device_opener opener = p_device->open();
+                if (not opener.is_open()) {
+                    std::cerr << "Could not open device: " << spak::describe_io_return(opener.result()) << std::endl;
+                    return return_code::cannot_open_device;
+                }
+                spak::hid_device_elements_enumerator elements = p_device->elements(kHIDPage_LEDs);
+                if (cmd.element >= elements.size()) {
+                    std::cerr << "Device has only " << elements.size() << " LED elements, cannot find LED number " << cmd.element << std::endl;
+                    return return_code::led_not_found;
+                }
+                if (cmd.action == cmdline::actions::set) {
+                    elements[cmd.element].value<CFIndex>() = cmd.value;
+                } else {
+                    spak::hid_device_element &element = elements[cmd.element];
+                    spak::hid_device_element_value<CFIndex> value = element.value<CFIndex>();
+                    if (value == element.logical_min()) {
+                        value = element.logical_max();
+                    } else {
+                        value = element.logical_min();
+                    }
+                }
+                return return_code::ok;
+            }
+        }
+    } catch (std::exception e) {
+        std::cerr << "Unknown exception: " << e.what() << std::endl;
+        return return_code::unknown_error;
+    }
 }
